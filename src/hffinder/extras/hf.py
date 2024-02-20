07a72e17ca5ee50feffbd36a15ad42e5fb9208eb
ftopal <fazilbtopal@gmail.com>
2024-02-20 12:00:01 +0100

    hf dataset config

---
+++ src/hffinder/extras/hf.py
@@ -0,0 +1,49 @@
+from typing import Any
+from datasets import Dataset, load_dataset
+from kedro.io import AbstractDataset
+import logging
+
+
+logger = logging.getLogger(__file__)
+
+
+class HFDataset(AbstractDataset):
+    def __init__(
+        self,
+        filepath: str = None,
+        dataset_name: str = None,
+        credentials: dict[Any] = None,
+        raise_error_if_push_hub_fails: bool = True
+    ):
+        self._filepath = filepath
+        self._dataset_name = dataset_name
+        self._credentials = credentials
+        self._raise_error_if_push_hub_fails = raise_error_if_push_hub_fails
+
+    def _load(self) -> Dataset:
+        try:
+            ds = Dataset.load_from_disk(self._filepath)
+        except FileNotFoundError:
+            ds = load_dataset(self._dataset_name)
+        return ds
+
+    def _save(self, data: Dataset) -> None:
+        logger.info("Saving to local disk.")
+        data.save_to_disk(self._filepath)
+
+        logger.info("Saving to HuggingFace Hub")
+        token = self._credentials.get('write')
+
+        try:
+            data.push_to_hub(self._dataset_name, token=token)
+        except BaseException as e:
+            logger.warning(f"Push to hub failed, provide credentials maybe?")
+            if self._raise_error_if_push_hub_fails:
+                raise e
+
+    def _describe(self) -> dict[str, Any]:
+        return {
+            "file_path": self._filepath,
+            "dataset_name": self._dataset_name,
+            "raise_error_if_push_hub_fails": self._raise_error_if_push_hub_fails,
+        }
