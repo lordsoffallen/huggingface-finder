e0d79ea564b0760e08b4eb69f06c2aba2e2049a2
ftopal <fazilbtopal@gmail.com>
2024-03-20 00:01:33 +0100

    Fix truncation

--- src/hffinder/pipelines/embeddings.py
+++ src/hffinder/pipelines/embeddings.py
@@ -26,7 +26,7 @@ def get_embeddings(
         text_list,
         padding='max_length',
         max_length=tokenizer.model_max_length,
-        truncation=False,
+        truncation=True,
         return_tensors="pt",
     )
 
